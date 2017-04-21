8ab2f4ee83852e3b9b6a5e1fd978a8f0aa878759
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-04-21 18:00:52 -0400

    Add normalization of dataset

---
+++ normalize.py
@@ -0,0 +1,16 @@
+from glob import glob
+import os
+import numpy as np
+import PIL.Image as Image
+
+data = glob(os.path.join("./data", "ift", "*.jpg"))
+
+for d in data:
+    img = np.array(Image.open(d))
+    if img.shape != (64, 64, 3):
+        print(img.shape)
+        img = np.repeat(img[:, :, None], axis=2, repeats=3)
+        Image.fromarray(img).save(d)
+
+
+all(len(d.shape) == 3 for d in data)
